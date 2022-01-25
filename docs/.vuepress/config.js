7bc71786c290c54307e6f084622c965e9e0da8dc
nerderlyne <shivanshityagi@icloud.com>
2022-01-26 00:11:17 +0530

    update config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "Kali",
+  description: `Kali docs.`,
+  themeConfig: {
+    repo: "kalidao/kali-docs",
+    editLinks: true,
+    editLinkTest: "Make a PR on GitHub to help improve this page.",
+    nav: [{ text: "Home", link: "/" }],
+  },
+  dest: "public",
+  sidebarDepth: 10,
+  sidebar: "auto",
+  smoothScroll: true,
+};
